a45e5b3c2b2382f8470a39b6e23845ab0a88b69a
DuvanP97 <duvan.a.pedroza.n@gmail.com>
2023-05-11 08:20:03 -0600

    add data mock

---
+++ src/constants/dataMock.ts
@@ -0,0 +1,105 @@
+export const navigation = [
+    { name: "Home", href: "/" },
+    { name: "Persons", href: "characters" },
+    { name: "Planets", href: "planets" },
+    { name: "Ships", href: "ships" },
+];
+
+export const people = [
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/characters/1.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/characters/2.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/characters/3.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/characters/4.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/characters/5.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/characters/6.jpg",
+  },
+];
+
+export const planets = [
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/planets/1.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/planets/2.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/planets/3.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/planets/4.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/planets/5.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/planets/6.jpg",
+  },
+];
+
+export const ships = [
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/starships/1.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/starships/2.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/starships/3.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/starships/4.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/starships/5.jpg",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl: "https://starwars-visualguide.com/assets/img/starships/6.jpg",
+  },
+];
