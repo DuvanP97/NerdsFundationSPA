f9d186593a328e9ef7d18a841bc71731bb8d77be
DuvanP97 <duvan.a.pedroza.n@gmail.com>
2023-05-11 10:13:24 -0600

    add data

--- src/constants/dataMock.ts
+++ src/constants/dataMock.ts
@@ -1,3 +1,5 @@
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+
 export const navigation = [
     { name: "Home", href: "/" },
     { name: "Persons", href: "characters" },
@@ -103,3 +105,109 @@ export const ships = [
     imageUrl: "https://starwars-visualguide.com/assets/img/starships/6.jpg",
   },
 ];
+
+export const characters = [
+  {
+    name: 'Luke Skywalker',
+    description: '19 BBY',
+    imageSrc: 'https://starwars-visualguide.com/assets/img/characters/1.jpg',
+    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+    href: 'character-detail',
+  },
+  {
+    name: 'Self-Improvement',
+    description: 'Journals and note-taking',
+    imageSrc: 'https://starwars-visualguide.com/assets/img/characters/2.jpg',
+    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+    href: 'character-detail',
+  },
+  {
+    name: 'Travel',
+    description: 'Daily commute essentials',
+    imageSrc: 'https://starwars-visualguide.com/assets/img/characters/3.jpg',
+    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+    href: 'character-detail',
+  },
+  {
+    name: 'Desk and Office',
+    description: 'Work from home accessories',
+    imageSrc: 'https://starwars-visualguide.com/assets/img/characters/4.jpg',
+    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+    href: 'character-detail',
+  },
+  {
+    name: 'Self-Improvement',
+    description: 'Journals and note-taking',
+    imageSrc: 'https://starwars-visualguide.com/assets/img/characters/5.jpg',
+    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+    href: 'character-detail',
+  },
+  {
+    name: 'Travel',
+    description: 'Daily commute essentials',
+    imageSrc: 'https://starwars-visualguide.com/assets/img/characters/6.jpg',
+    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+    href: 'character-detail',
+  },
+  {
+    name: 'Desk and Office',
+    description: 'Work from home accessories',
+    imageSrc: 'https://starwars-visualguide.com/assets/img/characters/7.jpg',
+    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+    href: 'character-detail',
+  },
+  {
+    name: 'Self-Improvement',
+    description: 'Journals and note-taking',
+    imageSrc: 'https://starwars-visualguide.com/assets/img/characters/8.jpg',
+    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+    href: 'character-detail',
+  },
+  {
+    name: 'Travel',
+    description: 'Daily commute essentials',
+    imageSrc: 'https://starwars-visualguide.com/assets/img/characters/9.jpg',
+    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+    href: 'character-detail',
+  },
+  {
+    name: 'Desk and Office',
+    description: 'Work from home accessories',
+    imageSrc: 'https://starwars-visualguide.com/assets/img/characters/10.jpg',
+    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+    href: 'character-detail',
+  },
+  {
+    name: 'Self-Improvement',
+    description: 'Journals and note-taking',
+    imageSrc: 'https://starwars-visualguide.com/assets/img/characters/11.jpg',
+    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+    href: 'character-detail',
+  },
+  {
+    name: 'Travel',
+    description: 'Daily commute essentials',
+    imageSrc: 'https://starwars-visualguide.com/assets/img/characters/12.jpg',
+    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+    href: 'character-detail',
+  },
+]
+
+export const dataCharacter = [
+  {
+    name: 'Push to deploy.',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: 'SSL certificates.',
+    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Database backups.',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: ServerIcon,
+  },
+]
